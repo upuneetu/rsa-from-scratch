af4c3285fa32a5b495d33ee3f2c27e7ac9e58139
pun97 <upuneetu@gmail.com>
2017-12-16 21:54:59 +0530

    added fermats test

--- cpp11/primality_tests.cpp
+++ cpp11/primality_tests.cpp
@@ -1,9 +1,11 @@
 #include "prime_helper.cpp"
+#include <set>
+#include <ctime>
+#include <cstdlib>
+bool naive_primality_test(int64_t n);
 
-bool naive_prime_tester(int64_t n);
 
-
-bool naive_prime_tester(int64_t n){
+bool naive_primality_test(int64_t n){
 	if(n%2==0)
 		return false;
 
@@ -11,6 +13,27 @@ bool naive_prime_tester(int64_t n){
 		if(n%(2*i+1)==0)
 			return false;
 	
+	return true;
+}
+
+bool fermats_primality_test(int64_t n,int64_t k=10){
+	if(n%2==0)
+		return false;
+
+	std::set<int64_t> checked_elements;
+	srand(time(NULL));
+	while(k>0) {
+		int64_t a = rand()%n;
+		if(gcd(a,n)!=0)
+			return false;
+		else if(checked_elements.find(a)!=std::set::end)
+			continue;
+		if(modular_exponentiation(a,n-1,n)!=1)
+			return false;
+		set.insert(a);
+		k--;		
+	}
+
 	return true;
 }
 
