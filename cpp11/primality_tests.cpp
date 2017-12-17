2da190d3862b8393d9e579cd3c343a8c4016f6dd
pun97 <upuneetu@gmail.com>
2017-12-17 21:26:19 +0530

    added solvay strassen and miller rabin primality tests

--- cpp11/primality_tests.cpp
+++ cpp11/primality_tests.cpp
@@ -1,11 +1,14 @@
 #include "prime_helper.cpp"
-#include <set>
 #include <ctime>
 #include <cstdlib>
+#include <cmath>
+#include <string>
 
 bool naive_primality_test(int64_t n);
 bool fermats_primality_test(int64_t n,int64_t k);
 bool solvay_strassen_primality_test(int64_t n,int64_t k);
+bool miller_rabin_primality_test(int64_t n, int64_t k);
+bool primality_test(int64_t n,std::string primality_test_string);
 
 bool naive_primality_test(int64_t n){
 	if(n%2==0)
@@ -24,21 +27,74 @@ bool fermats_primality_test(int64_t n,int64_t k=10){
 	if(n%2==0)
 		return false;
 
-	std::set<int64_t> checked_elements;
+	
 	srand(time(NULL));
 	while(k>0) {
 		int64_t a = rand()%(n-3)+2;
-		if(gcd(a,n)!=1){
-			return false;
-		}
-		else if(checked_elements.find(a) != checked_elements.end())
-			continue;
-		if(modular_exponentiation(a,n-1,n)!=1){
-			return false;
-		}
-		checked_elements.insert(a);
+		if(gcd(a,n)!=1||modular_exponentiation(a,n-1,n)!=1)
+			return false;	
 		k--;		
 	}
 
 	return true;
+}
+
+bool solvay_strassen_primality_test(int64_t n,int64_t k=10){
+	if(n==2)
+		return true;
+	if(n%2==0||n==1)
+		return false;
+
+	srand(time(NULL));
+	while(k>0) {
+		
+		int64_t a = rand()%(n-2)+1;
+		if((jacobi_symbol(a,n)+n)%n!=modular_exponentiation(a,(n-1)/2,n))
+			return false;
+		
+		k--;
+	}
+
+	return true;
+}
+
+bool miller_rabin_primality_test(int64_t n, int64_t k=10){
+
+	int64_t s = 0,d=n-1;
+	bool witness_flag;
+	while(d%2==0){
+		d = d >> 1;
+		s++;
+	}
+	
+	while(k){
+		k--;
+		int64_t p = s;
+		int64_t a = rand()%(n-2)+1;
+		if(modular_exponentiation(a,d,n)==1)
+			continue;
+		witness_flag = 1;
+		s--;
+		while(s>=0){
+			if(modular_exponentiation(a,(pow(2,s)*d),n)==n-1){
+				witness_flag = 0;
+				break;
+			}
+			s--;
+		}
+
+		if(witness_flag)
+			return false;
+		s = p;
+	}
+
+	return true;
+}
+
+
+bool primality_test(int64_t n,std::string primality_test_string){
+	if(primality_test_string.compare("solvay_strassen"))
+		return solvay_strassen(n);
+	if(primality_test_string.compare("miller_rabin"))
+		return miller_rabin(n);
 }
